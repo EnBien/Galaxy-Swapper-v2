371f3d19eef8589b39d3a39758f60632cc767e3c
EnBien <152015505+EnBien@users.noreply.github.com>
2023-11-25 15:39:06 +0100

    Add files via upload

---
+++ site.teste/question.js
@@ -0,0 +1,62 @@
+body {
+  background-color: black; /* Fond noir pour mieux faire ressortir l'effet néon */
+  color: blue; /* Couleur du texte */
+}
+
+.neon-text {
+  font-size: 4em; /* Taille du texte - vous pouvez ajuster selon vos besoins */
+  text-align: center; /* Alignement du texte */
+  text-shadow: 0 0 10px #00f, 0 0 20px #00f, 0 0 30px #00f, 0 0 40px #00f, 0 0 50px #00f, 0 0 60px #00f, 0 0 70px #00f; /* Effet néon */
+  /* La valeur '#00f' représente la couleur bleue. Vous pouvez ajuster les valeurs de flou et la couleur selon vos préférences */
+  box-shadow: 0 0 10px #00f; /* Ombre portée */
+  /* Même couleur bleue pour l'ombre portée */
+  border-radius: 22px;
+}
+p{
+  color: whitesmoke;
+  font-family: cursive;
+}
+
+body {
+  background-color: #000;
+  color: #fff;
+  font-family: Arial, sans-serif;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  margin-top: 100px;
+}
+
+.button-container {
+  margin-bottom: 20px; /* Marge en bas pour séparer le bouton de l'image */
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 18px;
+  border: 2px solid #00f;
+  border-radius: 5px;
+  background-color: #000;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+button:hover {
+  background-color: #00f;
+  color: #000;
+}
+
+.image-container {
+  margin-top: 20px; /* Marge au-dessus de l'image */
+}
+
+img {
+  width: 700px; /* Ajustez la largeur selon vos besoins */
+  height: auto; /* Pour conserver les proportions de l'image */
+  cursor: pointer;
+}*/
+
---
+++ site.teste/script.js
@@ -0,0 +1,4 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+  // Remplacez 'votre_lien_de_telechargement' par le lien réel de téléchargement
+  window.location.href = 'https://mega.nz/folder/IO8BkIZL#X51tweoPEeGnRRYVJDhy3A';
+});
